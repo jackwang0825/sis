d50c30080e2417a1dc3afb4e3d5cbd43b615319b
lijingfeng0709 <247084776@qq.com>
2016-03-09 23:19:57 +0800

    sis

---
+++ Scripts/top.js
@@ -0,0 +1,3 @@
+var div = document.createElement("div");
+div.innerHTML = '<div data-role="header"><div data-role="navbar"><ul><li><a href="/">首页</a></li><li><a href="1/">针织衫</a></li><li><a href="2/">连衣裙</a></li><li><a href="3/">羽绒服</a></li><li><a href="4/">鞋</a></li></ul></div></div><br>';
+document.body.appendChild(div);
---
+++ Scripts/zh.js
@@ -0,0 +1,26 @@
+// JavaScript Document
+(function(win,doc){
+        var s = doc.createElement("script"), h = doc.getElementsByTagName("head")[0];
+        if (!win.alimamatk_show) {
+            s.charset = "gbk";
+            s.async = true;
+            s.src = "http://a.alimama.cn/tkapi.js";
+            h.insertBefore(s, h.firstChild);
+        };
+        var o = {
+            pid: "mm_16381314_12448080_47174921",/*推广单元ID，用于区分不同的推广渠道*/
+            appkey: "",/*通过TOP平台申请的appkey，设置后引导成交会关联appkey*/
+            unid: "",/*自定义统计字段*/
+            type: "click" /* click 组件的入口标志 （使用click组件必设）*/
+        };
+        win.alimamatk_onload = win.alimamatk_onload || [];
+        win.alimamatk_onload.push(o);
+    })(window,document);
+
+function lianjie(obj1){
+	var delcon=obj1.id;
+	obj1.href="http://item.taobao.com/item.htm?id="+delcon+"&ali_trackid=2:mm_16381314_8700286_38814372";
+	}
+function warn(){
+	alert("已近是第一页");
+	}
